c55b7854aa0c1e20d2de495aec8715f45f321298
Steven <mitchs.patterson@gmail.com>
2019-07-20 23:08:06 -0600

    Node.js Project

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 5000
+require('dotenv').config();
+const { Pool } = require("pg")
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({connectionString: connectionString});
+const homepageController = require("./controller/controller.js");
 
-express()
+app
   .use(express.static(path.join(__dirname, 'public')))
+  .use(express.json())
+  .use(express.urlencoded({extended: true}))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/getBook', controller.getBook)
+  .get('/getalphaBook', controller.getalphaBook)
+  .post('/addBook', controller.addBook)
+  .post('/removeBook', controller.removeBook)
+  .post('/editBook', controller.editBook)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  
